cc446c84a60b6670d84db86e82a5b875f9ae142a
= <=>
2025-10-09 19:12:59 +0100

    refactor: Remove unused imports from SignupPage component

--- app/(main)/signup/page.tsx
+++ app/(main)/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import axios from "axios";
 
